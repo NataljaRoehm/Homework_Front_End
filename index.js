631bba6a4333a9d046fb5c513119b9cf6713b466
Natalja Röhm <roehm.natalia@gmail.com>
2023-09-30 22:30:37 +0200

    homework9

---
+++ index.js
@@ -0,0 +1,12 @@
+// Создайте массив с названиями городов "London", "Paris", "Berlin", "Madrid". 
+// Используя пройденные на занятии методы массива получите из массива новый массив, 
+// в котором элементы идут в обратной последовательности
+
+let cities = ["London", "Paris", "Berlin", "Madrid"];
+
+let reversCities = []
+
+for (let i = cities.length - 1; i >=0; i--) {
+     reversCities.push(cities[i]);
+}
+     console.log(reversCities);    
